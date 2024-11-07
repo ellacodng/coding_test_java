9a5a5fa2b34458d2db75321d027e9195acb90634
ellacodng <dayeon11@dankook.ac.kr>
2024-11-07 18:45:46 +0900

    commit

---
+++ src/Solution/Solution17.java
@@ -0,0 +1,16 @@
+package Solution;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution17 {
+    class Solution {
+        public String solution(String[] cards1, String[] cards2, String[] goal) {
+            Queue<String> cards1Queue = new ArrayDeque<>();
+            Queue<String> cards2Queue = new ArrayDeque<>();
+            Queue<String> goalQueue = new ArrayDeque<>();
+
+            return "yes";
+        }
+    }
+}
