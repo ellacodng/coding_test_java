1c6d51f0cc32bbccdabd52df10835464e22e1d78
ellacodng <dayeon11@dankook.ac.kr>
2024-11-07 19:50:06 +0900

    file name 설정

---
+++ src/08_Hash/Solution18.java
@@ -0,0 +1,14 @@
+package Solution;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Solution18 {
+        public String solution(String[] cards1, String[] cards2, String[] goal) {
+            Queue<String> cards1Queue = new ArrayDeque<>();
+            Queue<String> cards2Queue = new ArrayDeque<>();
+            Queue<String> goalQueue = new ArrayDeque<>();
+
+            return "yes";
+        }
+}
