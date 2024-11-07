98679c8a3a6f4c89fc5bff5e4e27cce3e8a342e1
ellacodng <dayeon11@dankook.ac.kr>
2024-11-07 17:11:04 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int [] progresses = {93, 30, 55};
-        int [] speeds = {1, 30, 5};
+
     }
 }
